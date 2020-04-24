f0bbec44839ac611079edec83f72a40da11e7055
Weact <lucas.druckes@numericable.fr>
2020-04-24 17:02:22 +0200

    Fin jeu

--- Scenes/Player/Direction.gd
+++ Scenes/Player/Direction.gd
@@ -10,16 +10,10 @@ func _input(event):
 	if event.is_action_pressed(inputs_node.input_map["MoveLeft"]):
 		dirLeft = 1
 		dirRight = 0
-		
-	#elif event.is_action_released(inputs_node.input_map["MoveLeft"]):
-	#	dirLeft = 0
 	
 	elif event.is_action_pressed(inputs_node.input_map["MoveRight"]):
 		dirRight = 1
 		dirLeft = 0
-	
-	#elif event.is_action_released(inputs_node.input_map["MoveRight"]):
-	#	dirRight = 0
 
 
 # Returns the direction of the robot
